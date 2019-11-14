36d87b8b66f9a6aff48137c38e5a58a6d2867f80
git <lukechaston@gmail.com>
2019-11-14 16:43:49 +0000

    Added DB to settings.

--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,11 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+        'db' => [
+            'host' => 'mysql:host=127.0.0.1;',
+            'dbName' => 'dbname=ToDoList',
+            'userName' => 'root',
+            'password' => 'password'
+        ],
     ],
 ];
