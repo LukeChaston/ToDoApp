9af983f5dca9f5acc30b2018bd6f627b992d853a
git <lukechaston@gmail.com>
2019-11-14 16:14:46 +0000

    Added function to controller to get all of the tasks.

--- src/Classes/Controllers/GetAllToDoController.php
+++ src/Classes/Controllers/GetAllToDoController.php
@@ -1,1 +1,20 @@
 <?php
+namespace LukeNamespace\Controllers;
+
+class GetAllToDoController
+{
+    private $Renderer;
+    private $TaskToDo;
+
+    public function __construct($Renderer, $TaskToDo)
+    {
+        $this->Renderer = $Renderer;
+        $this->TaskToDo = $TaskToDo;
+    }
+
+    public function __invoke($request, $response, $args)
+    {
+        $args['task'] = $this->TaskToDo->getTaskById($args['id']);
+        return $response->withJson($args['task'], 200);
+    }
+}
