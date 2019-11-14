4187af8742a4c73be10aca9a157f9704f8172ab8
git <lukechaston@gmail.com>
2019-11-14 16:41:44 +0000

    Created factory for Get All controller.

--- src/Classes/Factories/GetAllToDoControllerFactory.php
+++ src/Classes/Factories/GetAllToDoControllerFactory.php
@@ -1,1 +1,14 @@
 <?php
+namespace LukeNamespace\Factories;
+use LukeNamespace\Controllers\GetAllToDoController;
+use Psr\container\ContainerInterface;
+
+class GetAllToDoControllerFactory
+{
+    public function __invoke(ContainerInterface $container) : GetAllToDoController
+    {
+        $TaskToDo = $container->get('ToDoModel');
+        $renderer = $container->get('Renderer');
+        return new GetAllToDoController($TaskToDo, $renderer);
+    }
+}
